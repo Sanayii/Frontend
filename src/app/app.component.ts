95d728df11ec2395bd5367e15acdab97347aa00c
amalali20 <amal.ali.elbeshbeishy@gmail.com>
2025-04-20 11:05:48 +0200

    project init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './_component/header/header.component';
+import { ServicesComponent } from './_component/services/services.component';
+import { LandingComponent } from './_component/landing/landing.component';
+import { AboutComponent } from './_component/about/about.component';
+import { RegisterComponent } from './_component/register/register.component';
+import { LoginComponent } from './_component/login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent,LandingComponent,ServicesComponent,AboutComponent,RouterLink,
+    RegisterComponent,LoginComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './_component/register/register.component';
+import { LoginComponent } from './_component/login/login.component';
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent ,pathMatch:'full'},
+  { path: 'register', component: RegisterComponent,pathMatch:'full' },
+];
 
-export const routes: Routes = [];
+
