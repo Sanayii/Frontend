e5981432ef72b7deb7ffa7b446d82d68e5a23f2c
rania504 <khalifarania6@gmail.com>
2025-04-24 16:27:43 +0200

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './_component/header/header.component';
-import { ServicesComponent } from './_component/services/services.component';
-import { LandingComponent } from './_component/landing/landing.component';
-import { AboutComponent } from './_component/about/about.component';
-import { RegisterComponent } from './_component/register/register.component';
-import { LoginComponent } from './_component/login/login.component';
+import { CategoriesComponent } from './categories/categories.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HeaderComponent,LandingComponent,ServicesComponent,AboutComponent,RouterLink,
-    RegisterComponent,LoginComponent
-  ],
+  imports: [RouterOutlet, RouterLink,CategoriesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/categories/categories.component.ts
@@ -0,0 +1,40 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import * as bootstrap from 'bootstrap';
+import { FormsModule } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-categories',
+  imports: [CommonModule,FormsModule],
+  templateUrl: './categories.component.html',
+  styleUrl: './categories.component.css'
+})
+export class CategoriesComponent {
+  categories = [
+    {name: 'سباكة',image: 'images/plumbing.jpeg'},
+    {name: 'كهرباء',image: 'images/electrical.jpeg'},
+    {name: 'نجارة',image: 'images/joiner.jpeg'},
+    {name:'دهانات ونقاشة',image:'images/debate.jpeg'},
+    {name:'تركيب سيراميك وبلاط',image:'images/maintenance.jpeg'},
+    {name:'حدادة وألمنيوم',image:'images/blacksmith.jpeg'},
+    {name:'تنظيف وصيانة منازل',image:'images/cleaning.jpeg'},
+    {name:'تصميم وحدات ديكور',image:'images/decor.jpeg'},
+    {name:'إصلاح أجهزة كهربائية',image:'images/elec_repair.jpeg'},
+    {name:'صيانة المكيفات والتبريد',image:'images/conditioning.jpeg'}
+  ];
+
+  selectedCategory: string = "";
+
+  handleCategoryClick(serviceName: string) {
+    this.selectedCategory = serviceName;
+    const modal = document.getElementById('serviceModal');
+    if (modal) {
+      const bootstrapModal = new bootstrap.Modal(modal);
+      bootstrapModal.show();
+    }
+  }
+  submitRequestForm() {
+    alert(`Request submitted for: ${this.selectedCategory}`);
+  }
+}
